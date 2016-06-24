763f765178af86c804cddc68127b84fe91cfdd25
Noëlie Andrieu <noelieandrieu@gmail.com>
2016-06-24 10:28:57 +0200

    [fix] rebase issue

--- client/test/router_store.spec.js
+++ client/test/router_store.spec.js
@@ -184,116 +184,9 @@ describe.skip('Router Store', () => {
     ]);
     routerStore = require('../app/stores/router_store');
 
-  describe('Actions', () => {
-    it('ROUTE_INITIALIZE', () => {
-      // Fire two events to allow the initialization of the router store:
-      // ADD_ACCOUNT_SUCCESS and RECEIVE_RAW_MESSAGES. The first one
-      // adds an account and the second one adds messages. That way the router
-      // can build URLs and performs its initializaton like if data are arleady
-      // present in the application.
-      dispatcher.dispatch({
-        type: ActionTypes.RECEIVE_RAW_MESSAGES,
-        value: [fixtures.message1, fixtures.message2],
-      });
-      dispatcher.dispatch({
-        type: ActionTypes.ROUTES_INITIALIZE,
-        value: fixtures.router,
-      });
-      assert.equal(routerStore.getRouter(), fixtures.router);
-    });
-    it('ROUTE_CHANGE (no accounts)', () => {
-      // First we test that the new account action is automatically selected
-      // when the ROUTE_CHANGE event is fired while no account is added to the
-      // store.
-      dispatcher.dispatch({
-        type: ActionTypes.ROUTE_CHANGE,
-        value: { },
-      });
-      assert.equal(routerStore.getAction(), AccountActions.CREATE);
-    });
-    it('ROUTE_CHANGE (accounts)', () => {
-      dispatcher.dispatch({
-        type: ActionTypes.ADD_ACCOUNT_SUCCESS,
-        value: { account: _.clone(fixtures.account) },
-      });
-      dispatcher.dispatch({
-        type: ActionTypes.ROUTE_CHANGE,
-        value: {
-          accountID: fixtures.account.id,
-          mailboxID: fixtures.account.inboxMailbox,
-          tab: 'selected',
-          action: MessageActions.SHOW,
-          conversationID: fixtures.message1.conversationID,
-          messageID: fixtures.message1.id,
-          filter: '',
-        },
-      });
-      assert.equal(routerStore.getAction(), MessageActions.SHOW);
-      assert.equal(routerStore.getAccountID(), fixtures.account.id);
-      assert.equal(routerStore.getMailboxID(), fixtures.account.inboxMailbox);
-      assert.equal(routerStore.getMessageID(), fixtures.message1.id);
-      assert.equal(routerStore.getConversationID(),
-                   fixtures.message1.conversationID);
-      assert.isNull(routerStore.getFilter().value);
-      assert.equal(routerStore.getCurrentURL({ isServer: false }),
-                   '#mailbox/mb1/c1/i1');
-
-      dispatcher.dispatch({
-        type: ActionTypes.ROUTE_CHANGE,
-        value: {
-          accountID: fixtures.account.id,
-          mailboxID: fixtures.account.inboxMailbox,
-          tab: 'selected',
-          action: MessageActions.SHOW_ALL,
-        },
-      });
-      assert.equal(routerStore.getAction(), MessageActions.SHOW_ALL);
-      assert.equal(routerStore.getAccountID(), fixtures.account.id);
-      assert.equal(routerStore.getMailboxID(), fixtures.account.inboxMailbox);
-      assert.isNull(routerStore.getFilter().value);
-      assert.equal(routerStore.getCurrentURL({ isServer: false }),
-                   '#mailbox/mb1');
-    });
-    it.skip('ADD_ACCOUNT_SUCCESS', (done) => {
-      dispatcher.dispatch({
-        type: ActionTypes.ADD_ACCOUNT_SUCCESS,
-        value: { account: _.clone(fixtures.account) },
-      });
-      setTimeout(() => {
-        assert.equal(routerStore.getAction(), MessageActions.SHOW_ALL);
-        assert.equal(routerStore.getAccountID(), fixtures.account.id);
-        assert.equal(routerStore.getMailboxID(), fixtures.account.inboxMailbox);
-        assert.equal(routerStore.getSelectedTab(), 'account');
-        assert.equal(currentURL, `#mailbox/${fixtures.account.inboxMailbox}`);
-        assert.equal(routerStore.getCurrentURL({ isServer: false }),
-                     `#mailbox/${fixtures.account.inboxMailbox}`);
-        done();
-      }, 5000);
-    }); // .timeout(6000);
-    // Uncomment previous line, if you decide to unskip this test.
-    it('MESSAGE_FETCH_SUCCESS', () => {
-      dispatcher.dispatch({
-        type: ActionTypes.MESSAGE_FETCH_SUCCESS,
-        value: {
-          lastPage: fixtures.lastPage,
-          result: {},
-          timestamp: new Date(),
-        },
-      });
-      assert.deepEqual(routerStore.getLastPage(), fixtures.lastPage);
-    });
-    it('DISPLAY_MODAL', () => {
-      dispatcher.dispatch({
-        type: ActionTypes.DISPLAY_MODAL,
-        value: fixtures.modal,
-      });
-      const params = routerStore.getModalParams();
-      assert.deepEqual(params, fixtures.modal);
-    });
-    it('HIDE_MODAL', () => {
-      dispatcher.dispatch({ type: ActionTypes.HIDE_MODAL });
-      const params = routerStore.getModalParams();
-      assert.isNull(params);
+    dispatcher.dispatch({
+      type: ActionTypes.ROUTE_INITIALIZE,
+      value: router,
     });
     it('MESSAGE_TRASH_SUCCESS', () => {
       dispatcher.dispatch({
@@ -309,34 +202,9 @@ describe.skip('Router Store', () => {
         },
       });
 
-
-      // FIXME: this event require parameters that are never used.
-      // We should check if they are required by another store.
-      dispatcher.dispatch({
-        type: ActionTypes.MESSAGE_TRASH_SUCCESS,
-        value: {
-          target: '',
-          updated: false,
-          ref: '',
-        },
-      });
-      // Test if it goes to next conversation.
-      const mailboxID = fixtures.account.inboxMailbox;
-      const conversationID = fixtures.message2.conversationID;
-      const messageID = fixtures.message2.id;
-      assert.equal(routerStore.getCurrentURL(),
-        `mailbox/${mailboxID}/${conversationID}/${messageID}/`);
-    });
-    it('REMOVE_ACCOUNT_SUCCESS', () => {
-      dispatcher.dispatch({
-        type: ActionTypes.REMOVE_ACCOUNT_SUCCESS,
-        value: fixtures.account.id,
-      });
-      assert.equal(routerStore.getAction(), AccountActions.CREATE);
-      // FIXME: these tests should failed, because the account was removed.
-      // assert.isUndefined(routerStore.getAccountID());
-      // assert.isUndefined(routerStore.getMailboxID());
-      assert.equal(routerStore.getSelectedTab(), 'account');
+    dispatcher.dispatch({
+      type: ActionTypes.ADD_ACCOUNT_SUCCESS,
+      value: { account: fixtures.account },
     });
 
   });
@@ -595,7 +463,6 @@ describe.skip('Router Store', () => {
   //   });
   // });
 
-
   describe('getStates related to:', () => {
 
     // TODO: tester lorsqu'on ne trouve aucun comptes
@@ -754,29 +621,29 @@ describe.skip('Router Store', () => {
             routerStore.isDraft(map(fixtures.message4)));
         });
 
-        it('getMailboxTotal', () => {
-          // TODO: faire des tests d'ajout et de suppression ici
-          // TODO: faire les tests des compteurs des mailbox flagged ici
-          assert.equal(
-            routerStore.getMailboxTotal(),
-            fixtures.account.mailboxes[0].nbTotal
-          );
-          //   changeRoute({ });
-          //   assert.equal(
-          //     routerStore.getMailboxTotal(),
-          //     fixtures.account.mailboxes[0].nbTotal
-          //   );
-          //   changeRoute({ flags: MessageFilter.UNSEEN });
-          //   assert.equal(
-          //     routerStore.getMailboxTotal(),
-          //     fixtures.account.mailboxes[0].nbUnread
-          //   );
-          //   changeRoute({ flags: MessageFilter.FLAGGED });
-          //   assert.equal(
-          //     routerStore.getMailboxTotal(),
-          //     fixtures.account.mailboxes[0].nbFlagged
-          //   );
-        });
+        // it('getMailboxTotal', () => {
+        //   // TODO: faire des tests d'ajout et de suppression ici
+        //   // TODO: faire les tests des compteurs des mailbox flagged ici
+        //   assert.equal(
+        //     routerStore.getMailboxTotal(),
+        //     fixtures.account.mailboxes[0].nbTotal
+        //   );
+        //   //   changeRoute({ });
+        //   //   assert.equal(
+        //   //     routerStore.getMailboxTotal(),
+        //   //     fixtures.account.mailboxes[0].nbTotal
+        //   //   );
+        //   //   changeRoute({ flags: MessageFilter.UNSEEN });
+        //   //   assert.equal(
+        //   //     routerStore.getMailboxTotal(),
+        //   //     fixtures.account.mailboxes[0].nbUnread
+        //   //   );
+        //   //   changeRoute({ flags: MessageFilter.FLAGGED });
+        //   //   assert.equal(
+        //   //     routerStore.getMailboxTotal(),
+        //   //     fixtures.account.mailboxes[0].nbFlagged
+        //   //   );
+        // });
     });
 
     // it('hasNextPage', () => {
